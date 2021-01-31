fa1f13dac750bd9036a42fedc2cc17b481172ade
Harun Kilic <harunkilic@live.dk>
2021-01-31 04:17:23 +0100

    Update

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.REDIS_HOST) {
     throw new Error("REDIS_HOST must be defined!");
   }
